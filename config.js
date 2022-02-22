944ef3913d35a374144d3a831be4cfc9743ad129
Astroth1984 <elmounir.othmane666@gmail.com>
2022-02-22 18:38:23 +0100

    Adding Configurations

---
+++ config.js
@@ -0,0 +1,46 @@
+/*
+ * Create and exportconfiguration variables
+ *
+ */
+
+// var enivironments = {
+//     'staging': {
+//         'port': 3000,
+//         'envName': 'staging'
+//     }
+//     'production': {
+//         'port': 5000,
+//         'envName': 'production'
+//     }
+// }
+
+
+// Container for all the environment
+var environments = {};
+
+// Staging(default) envirenment
+environments.staging = {
+    'port': 3000,
+    'envName': 'staging'
+};
+
+// Production envirenment
+environments.production = {
+    'port': 5000,
+    'envName': 'production'
+};
+
+// Determine which envirenment was passed as a command-line argument
+var currentEnv = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+/*
+ *
+ * Check that the currentEnv is one of the environments object
+ * if not set to default env which is staging env
+ *
+ */
+var envToExport = typeof(environments[currentEnv]) == 'object' ? environments[currentEnv] : environments.staging;
+
+// Export the module
+
+module.exports = envToExport;
--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 var http = require('http');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./config');
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res) {
@@ -93,8 +94,8 @@ var server = http.createServer(function(req, res) {
 });
 
 // Start the server
-server.listen(3000, function() {
-    console.log('The server is up and running now');
+server.listen(config.port, function() {
+    console.log('The server is up and running now on port ' + config.port + " in " + config.envName + " mode");
 });
 
 // Define all the handlers
