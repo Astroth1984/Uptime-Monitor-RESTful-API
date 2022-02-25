baf5cc2540336dfa97cec6d4b690a2099fbe7cc2
Astroth1984 <elmounir.othmane666@gmail.com>
2022-02-25 16:12:39 +0100

    Service /tokens : POST Request handler

--- index.js
+++ index.js
@@ -134,5 +134,6 @@ var unifiedServer = function(req, res) {
 // Define the request router
 var router = {
     'ping': handlers.ping,
-    'users': handlers.users
+    'users': handlers.users,
+    'tokens': handlers.tokens
 };
